f897ef10a12e88bf4de0fc3ac000438b0b2ee2e6
Niall Quigley <niallquigley92@gmail.com>
2019-07-06 20:01:29 +0100

    Basic Map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends React.Component {
+  renderMarkers = () => {
+    const { data } = this.props;
+    return data.map(d => (
+      <Marker
+
+        position={{ lat: d.lat, lng: d.lon }}
+      />
+    ))
+  }
+
   render() {
+    const { google, data } = this.props;
     return (
-      <Map google={this.props.google} zoom={14}>
-        <Marker onClick={this.onMarkerClick}
-          name={'Current location'} />
+      <Map
+      google={google}
+      zoom={14}
+      initialCenter={{
+        lat: data[0].lat,
+        lng: data[0].lon
+      }}>
+        {this.renderMarkers()}
       </Map>
     );
   }
