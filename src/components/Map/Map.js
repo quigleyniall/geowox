052f69ec1a597307e6e18440ff019136c944ed09
Niall Quigley <niallquigley92@gmail.com>
2019-07-06 18:11:56 +0100

    Map

---
+++ src/components/Map/Map.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+export class MapContainer extends React.Component {
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+        <Marker onClick={this.onMarkerClick}
+          name={'Current location'} />
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: process.env.MAP_API_KEY
+})(MapContainer)
