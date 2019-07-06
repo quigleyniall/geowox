1b5542c97fd4bed732543c04ede6cd37572e0dde
Niall Quigley <niallquigley92@gmail.com>
2019-07-06 15:43:05 +0100

    Home

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = ({
+      articles: []
+    })
+  }
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default Home;
